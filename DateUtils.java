188d33abb8fe031232e17f69f848e024298567dd
Dinesh <inamdardinesh29@gmail.com>
2023-03-27 15:21:16 +0530

    CS-Dateutils class

---
+++ DateUtils.java
@@ -0,0 +1,12 @@
+package Starpattern;
+
+public class DateUtils {
+
+	public static void main(String[] args) 
+	{
+               public void dateTOConvert()
+               {
+               System.out.println("Hello!!! in trhe world of git hub");
+               }
+      }
+}
